ce418302b0c044c50a58c3ad90f39814dd631c72
Rushda Rasheed <rushirasheed42@gmail.com>
2025-05-01 23:22:22 +0500

    Updated next.config.mjs with new config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,30 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+
+// };
+
+// export default nextConfig;
+
+
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-};
-
-export default nextConfig;
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/user/dashboard',
+          permanent: false, // use true if you want a permanent 308 redirect
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
